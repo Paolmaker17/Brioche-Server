811bdad50e38621cbff6881001eaadca00ee465b
Brioches <brioches@schipao.ftp.sh>
2026-03-09 13:35:46 +0100

    Serve websockets on port 1810

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-Deno.serve((req) => {
+Deno.serve({ port: 1810 }, (req) => {
   if (req.headers.get("upgrade") !== "websocket") {
     return new Response(null, { status: 501 });
   }
   const { socket, response } = Deno.upgradeWebSocket(req);
-  socket.onopen =  () => {
+  socket.onopen = () => {
     console.log("a client connected!");
   };
   socket.onmessage = (event) => {
